c650851d40a84a41ef57366f8ce2fd37cb048771
Manh Hung <lengocmanhhunh@gmail.com>
2026-01-04 23:20:39 +0700

    Refactor TCP_Object for improved socket handling

--- src/prepare/TCP_Object.java
+++ src/prepare/TCP_Object.java
@@ -4,9 +4,9 @@
  */
 package prepare;
 
-import TCP.Customer;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 
@@ -14,48 +14,35 @@ import java.net.Socket;
  *
  * @author dinhh
  */
+
+class ObjectClass implements Serializable{
+    private static final long serialVersionUID = 20150711L;
+}
+
 public class TCP_Object {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-        String serverHost = "203.162.10.109"; // hoặc IP server
-        int serverPort = 2209;
-        String studentCode = "B22DCDT147"; // đổi theo mã sinh viên của bạn
-        String qCode = "WOTt5fbM";
-        
-        try(Socket socket = new Socket())
-        {
-            socket.connect(new InetSocketAddress(serverHost, serverPort), 5000);
+        try(Socket socket = new Socket()) {
+            
+            socket.connect( new InetSocketAddress("203.162.10.109", 2208) );
             socket.setSoTimeout(5000);
-
-            ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
-            ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
             
-            // --- 1) Gửi mã sinh viên + qCode ---
-            String request = studentCode + ";" + qCode;
-            oos.writeObject(request);
-            oos.flush();
-            System.out.println("Sent: " + request);
+            ObjectInputStream in = new ObjectInputStream( socket.getInputStream() );
+            ObjectOutputStream out = new ObjectOutputStream( socket.getOutputStream() );
             
-             // --- 2) Nhận đối tượng Customer từ server ---
-            Customer customer = (Customer) ois.readObject(); //ép về kiểu objct mong muốn
-            System.out.println("Received customer:");
-            System.out.println(customer);
+            //send
+            String sendString = "Hello";
+            out.writeObject(sendString);
+            out.flush();
             
-            // Gửi đối tượng đã chuẩn hóa về server
-            oos.writeObject(customer);
-            oos.flush();
-            System.out.println("Sent formatted customer.");
-
-            // --- 4) Đóng kết nối ---
-            socket.close();
-            System.out.println("Connection closed.");
+            //receive
+            ObjectClass obj = (ObjectClass) in.readObject();
             
-        }
-        catch(Exception e)
-        {
+            
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
